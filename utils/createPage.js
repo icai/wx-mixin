4c692a94d6f261f5758e377c215c951ede90ca40
Terry Cai <gidcai@gmail.com>
2019-03-04 13:17:11 +0800

    update createPage

--- utils/createPage.js
+++ utils/createPage.js
@@ -4,7 +4,7 @@
 
 const isArray = v => Array.isArray(v);
 const isFunction = v => typeof v === 'function';
-const noop = function () {};
+const noop = function () { };
 
 // 借鉴redux https://github.com/reactjs/redux
 function compose(...funcs) {
@@ -18,40 +18,68 @@ function compose(...funcs) {
 
   const last = funcs[funcs.length - 1];
   const rest = funcs.slice(0, -1);
-  return (...args) => rest.reduceRight((composed, f) => f(composed), last(...args));
+  return (...args) => {
+    return rest.reduceRight((composed, f) => f(composed), last(...args))
+  };
+}
+
+/**
+ * 方法存在返回值
+ * @param  {...any} funcs 
+ */
+function composeCatch(...funcs) {
+  if (funcs.length === 0) {
+    return arg => arg;
+  }
+  if (funcs.length === 1) {
+    return funcs[0];
+  }
+  // const last = funcs[funcs.length - 1];
+  // const rest = funcs.slice(0, -1);
+  return (...args) => {
+    let s;
+    let k = {};
+    for (let index = funcs.length - 1; index >= 0; index--) {
+      const item = funcs[index];
+      if(isFunction(item)) {
+        k = item(...args);
+        if(k !== undefined) {
+          s = k;
+        }
+      }
+    }
+    return s;
+  };
 }
 
 
 // 页面堆栈
-const pagesStack = getApp().$pagesStack;
+// const pagesStack = getApp().$pagesStack;
 
 const PAGE_EVENT = ['onLoad', 'onReady', 'onShow', 'onHide', 'onUnload', 'onPullDownRefresh', 'onReachBottom', 'onShareAppMessage'];
 const APP_EVENT = ['onLaunch', 'onShow', 'onHide', 'onError'];
 
 const onLoad = function (opts) {
   // 把pageModel放入页面堆栈
-  pagesStack.addPage(this);
+  //  pagesStack.addPage(this);
 
-  this.$invoke = (pagePath, methodName, ...args) => {
-    pagesStack.invoke(pagePath, methodName, ...args);
-  };
+  //   this.$invoke = (pagePath, methodName, ...args) => {
+  //     pagesStack.invoke(pagePath, methodName, ...args);
+  //   };
 
   this.onBeforeLoad(opts);
   this.onNativeLoad(opts);
   this.onAfterLoad(opts);
 };
 
-const getMixinData = mixins => {
+const getMixinData = (mixins, type = 'data') => {
   let ret = {};
-
   mixins.forEach(mixin => {
-    let { data={} } = mixin;
-
+    let data = mixin[type] || {};
     Object.keys(data).forEach(key => {
       ret[key] = data[key];
     });
   });
-
   return ret;
 };
 
@@ -59,7 +87,7 @@ const getMixinMethods = mixins => {
   let ret = {};
 
   mixins.forEach(mixin => {
-    let { methods={} } = mixin;
+    let { methods = {} } = mixin;
 
     // 提取methods
     Object.keys(methods).forEach(key => {
@@ -87,6 +115,60 @@ const getMixinMethods = mixins => {
   return ret;
 };
 
+const getMixinDataAuto = (mixins, type = 'dataBinder') => {
+  let ret = {};
+  mixins.forEach(mixin => {
+    let data = mixin[type] || {};
+    Object.keys(data).forEach(key => {
+      if (ret[key]) {
+        ret[key] = ret[key].concat(data[key]);
+      } else {
+        ret[key] = [data[key]];
+      }
+    });
+  });
+  return ret;
+}
+
+
+const mixMethodsAuto = (mixinMethods, pageConf) => {
+  Object.keys(mixinMethods).forEach(key => {
+    let methodsList = mixinMethods[key];
+    if (pageConf[key]) {
+      methodsList.push(pageConf[key]);
+    }
+    pageConf[key] = (function () {
+      return function (...args) {
+        // 合并为一次 一次插入
+        // dataBinder: {
+        //   videoplay$vid: "vid"
+        //   videoplay$vid: "mixinvid"
+        // }
+        let strs = methodsList.filter(item =>  typeof item == 'string');
+        let funcs = methodsList.filter(item =>  typeof item == 'function');
+        return composeCatch(...[strs,...funcs].reverse().map(f => {
+          if(isFunction(f)) {
+            return f.bind(this)
+          } else {
+            // dataBinder 绑定合并
+            return (v) => {
+              let s = {};
+              f.forEach(it=> {
+                s[it] = v
+              })
+              // 绑定合并 一次插入
+              this.setData(s);
+            }
+          }
+        }))(...args);
+      };
+    })();
+  });
+
+  return pageConf;
+};
+  
+
 /**
  * 重复冲突处理借鉴vue:
  * data, methods会合并，组件自身具有最高优先级，其次mixins中后配置的mixin优先级较高
@@ -100,7 +182,7 @@ const mixData = (minxinData, nativeData) => {
       nativeData[key] = minxinData[key];
     }
   });
-  
+
   return nativeData;
 };
 
@@ -116,7 +198,11 @@ const mixMethods = (mixinMethods, pageConf) => {
 
       pageConf[key] = (function () {
         return function (...args) {
-          compose(...methodsList.reverse().map(f => f.bind(this)))(...args);
+          if(key != 'onShareAppMessage') {
+            compose(...methodsList.reverse().map(f => f.bind(this)))(...args);
+          } else {
+            return composeCatch(...methodsList.reverse().map(f => f.bind(this)))(...args);
+          }
         };
       })();
     }
@@ -143,7 +229,7 @@ export default pageConf => {
   let onNativeLoad = pageConf.onLoad || noop;
   let nativeData = pageConf.data || {};
 
-  let minxinData = getMixinData(mixins);
+  let minxinData = getMixinData(mixins, 'data');
   let mixinMethods = getMixinMethods(mixins);
 
   Object.assign(pageConf, {
@@ -154,7 +240,17 @@ export default pageConf => {
     onNativeLoad,
   });
 
+
   pageConf = mixMethods(mixinMethods, pageConf);
 
-  return pageConf;
+  // 提供方法跨 mixins 调用
+  pageConf.__callMixinMethod = function(method, ...arg) {
+    if( isFunction(this[method]) ) {
+      this[method](...arg);
+    } else {
+      console.warn(`${method} 在当前Page上没有定义，请检查`);
+    }
+  }
+
+  return Page(pageConf);
 };
